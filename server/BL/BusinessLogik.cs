a94454af77e1308cfe0f3640cb7d389226c56048
BojeUbeyteMenya <mralexgold01@gmail.com>
2023-04-26 18:40:38 +1000

    ну крч тут функция, котораая по id возвращает + прописаны условия че делать надо

--- server/BL/BusinessLogik.cs
+++ server/BL/BusinessLogik.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using DataBase.BD;
@@ -25,6 +27,47 @@ namespace server.BL
                 }
             }
             return (user);
+        }
+
+        public static string ChooseFunction(string ip, int port, IPEndPoint tcpEndPoint, Socket tcpSocket)
+        {
+
+            
+            while (true)
+            {
+
+                // класс конечной точки (точка подключения), принимает Ip and Port
+
+                 // сокет объявляем, через него все проходит + прописываем дефолтные характеристики для TCP
+                tcpSocket.Bind(tcpEndPoint); // Связываем сокет с конечной точкой (кого нужно слушать)
+                tcpSocket.Listen(100); // кол-во челов, которые могут подключиться
+
+
+
+                // обработчик на прием сообщения 
+                var listener = tcpSocket.Accept(); //новый сокет, который обрабатывает клиента
+                var buffer = new byte[256]; // массив байтов, куда будут приниматься сообщения
+                var size = 0;
+                var choice = new StringBuilder();
+
+                do
+                {
+                    size = listener.Receive(buffer); // в size записывается размерность реально полученных байт
+                    choice.Append(Encoding.UTF8.GetString(buffer, 0, size)); // переводим и записываем текст
+                }
+                while (listener.Available > 0);
+
+
+
+
+
+
+                listener.Shutdown(SocketShutdown.Both); // отключаем и у клиента, и у сервера
+                listener.Close(); // закрываем
+
+                return (choice.ToString());
+                break;
+            }
         }
     }
 }
