c72e1f0baf17e7d4e8f61ab09f4c1669a94bb8ff
t00sh0rt <maksimfpm28@gmail.com>
2023-05-04 00:41:23 +1000

    пока что ниче такой коммит

--- server/BL/BusinessLogik.cs
+++ server/BL/BusinessLogik.cs
@@ -5,49 +5,57 @@ using System.Net.Sockets;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
-using DataBase.BD;
+using System.Drawing;
 
-namespace server.BL
+namespace server
 {
     internal class BusinessLogik
     {
-        public static string FindUser(string bookingId, DataBase.BD.DataBase dataBase) //возвращает юзера по id
+        public static bool TryReceive(Socket listener, byte[] urByte)
         {
-            string user = "0";
-
+            try
+            {
+                listener.Receive(urByte);
+                //Console.WriteLine("Connected!");
+            }
+            catch (SocketException e)
+            {   //комменты для того чтобы узнать ошибку... алгоритм взял с сайта майкрософака...
+                //Console.WriteLine("Source : " + e.Source);
+                //Console.WriteLine("Message : " + e.Message); 
+                return false;
+            }
+            catch (Exception e)
+            {
+                //Console.WriteLine("Source : " + e.Source);
+                //Console.WriteLine("Message : " + e.Message);
+                return false;
+            }
+            return true;
+        }
+        public static string FindUserToSend(string bookingId, DataBase dataBase) //возвращает юзера по id
+        {
             for (int i = 0; i < dataBase.userobject.users.Length; i++)
             {
-                if (dataBase.userobject.users[i].id == Convert.ToInt64(bookingId))
+                if (dataBase.userobject.users[i].id == bookingId)
                 {
-                    user = DataBase.BD.DataBase.GetCurrentUserString(dataBase.userobject.users[i]);
-                    break;
-                }
-                else if (dataBase.userobject.users[i].id != Convert.ToInt64(bookingId))
-                {
-                    user = "ошибка";
+                    string user = DataBase.GetCurrentUserString(dataBase,bookingId);
+                    return (user);
                 }
             }
-            return (user);
+            return null;
         }
 
-        public static string FindRoom(string roomId, DataBase.BD.DataBase dataBase) //возвращает комнату по ее id
+        public static string GetRoom(string roomId, DataBase dataBase) //возвращает комнату по ее id
         {
-            string roomx = "0";
-
             for (int i = 0; i < dataBase.roomobject.rooms.Length; i++)
             {
-                if (dataBase.roomobject.rooms[i].id == Convert.ToInt64(roomId))
+                if (dataBase.roomobject.rooms[i].id == roomId)
                 {
-                    roomx = DataBase.BD.DataBase.GetCurrentUserString(dataBase.userobject.users[i]);
-                    break;
-                }
-                else if (dataBase.userobject.users[i].id != Convert.ToInt64(roomId))
-                {
-                    roomx = "ошибка";
+                    string roomx = DataBase.GetCurrentRoomString(dataBase, roomId);
+                    return (roomx);
                 }
             }
-            return (roomx);
-        
+            return null;
         }
     }
 }
